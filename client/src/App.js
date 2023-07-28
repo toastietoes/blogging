d7634f290131845f1810f31cb2e4ad14144f969a
toastietoes <juliejyyang@gmail.com>
2023-07-28 14:06:43 -0400

    added images and text

---
+++ client/src/App.js
@@ -0,0 +1,45 @@
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <header>
+        <a href="" className = "logo">MyBlog</a>
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </nav>
+      </header>
+      <div className="post">
+        <div className="image">
+        <img src="https://m.media-amazon.com/images/I/7107Y0VKCZL._AC_UF1000,1000_QL80_.gif" alt=""/>
+        </div>
+        <div className="text">
+          <h2>Steppenwolf</h2>
+          <p>my favourite book</p>
+        </div>
+      </div>
+      <div className="post">
+        <div className="image">
+        <img src="https://m.media-amazon.com/images/I/7107Y0VKCZL._AC_UF1000,1000_QL80_.gif" alt=""/>
+        </div>
+        <div className="text">
+          <h2>Steppenwolf</h2>
+          <p>my favourite book</p>
+        </div>
+      </div>
+      <div className="post">
+        <div className="image">
+        <img src="https://m.media-amazon.com/images/I/7107Y0VKCZL._AC_UF1000,1000_QL80_.gif" alt=""/>
+        </div>
+        <div className="text">
+          <h2>Steppenwolf</h2>
+          <p>my favourite book</p>
+        </div>
+      </div>
+    </main>
+    
+  );
+}
+
+export default App;
