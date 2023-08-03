1a440bf289dc3170998ad3913d51237acc37db67
toastietoes <juliejyyang@gmail.com>
2023-08-03 16:08:57 -0400

    mongoose connected

--- api/index.js
+++ api/index.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require("mongoose");
 const app = express();
 
-app.use(cors())
+app.use(cors());
+app.use(express.json());
 
-app.post('/register', (req, res) => {
-    res.json('test ok3');
+await mongoose.connect('mongodb+srv://toastietoes:<90lYNFRO9J9IEZdA>@cluster0.qc6lhkm.mongodb.net/?retryWrites=true&w=majority');
+
+app.post('/register', (req,res) => {
+    const {username,password} = req.body;
+    res.json({requestData:{username,password}});
 });
 
 app.listen(4000);
+
