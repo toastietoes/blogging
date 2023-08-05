f08a6b6d9ac0b86105c5d5db57d6f2e04703689b
toastietoes <juliejyyang@gmail.com>
2023-08-04 23:37:51 -0400

    registration and login

--- api/index.js
+++ api/index.js
@@ -3,6 +3,9 @@ const cors = require('cors');
 const mongoose = require("mongoose");
 const app = express();
 const User = require('./models/User');
+const bcrypt = require('bcryptjs');
+
+const salt = bcrypt.genSaltSync(10);
 
 app.use(cors());
 app.use(express.json());
@@ -11,9 +14,23 @@ mongoose.connect('mongodb+srv://toastietoes:90lYNFRO9J9IEZdA@cluster0.qc6lhkm.mo
 
 app.post('/register', async (req,res) => {
     const {username,password} = req.body;
-    const userDoc = await User.create({username,password});
-    res.json(userDoc);
+    try{
+        const userDoc = await User.create({
+            username,
+            password:bcrypt.hashSync(password,salt),
+        });
+        res.json(userDoc);
+    } catch(e) {
+        console.log(e);
+        res.status(400).json(e);
+    }
 });
 
+app.post('/login', async (req,res) => {
+    const {username, password} = req.body;
+    const userDoc = await User.findOne({username});
+    const passOk = bcrypt.compareSync(password, userDoc.password);
+    res.json(passOk)
+})
 app.listen(4000);
 
--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -6,11 +6,16 @@ export default function RegisterPage(){
     const [password, setPassword] = useState('');
     async function register(ev) {
         ev.preventDefault();
-        await fetch('http://localhost:4000/register', {
+        const response = await fetch('http://localhost:4000/register', {
             method: 'POST',
             body: JSON.stringify({username,password}),
             headers: {'Content-Type': 'application/json'},
         })
+        if (response.status === 200) {
+            alert('registration successful')
+        } else {
+            alert('registration failed')
+        }
     }
     return(
     <form className="register" onSubmit={register}>
