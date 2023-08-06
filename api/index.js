5017ddfac68a4e1e51437fee35876f3eb7cbb8bb
toastietoes <juliejyyang@gmail.com>
2023-08-06 08:36:18 -0400

    adding module

--- api/index.js
+++ api/index.js
@@ -4,10 +4,12 @@ const mongoose = require("mongoose");
 const app = express();
 const User = require('./models/User');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const salt = bcrypt.genSaltSync(10);
+const secret = 'fewfaefawefwfawf';
 
-app.use(cors());
+app.use(cors({credentials:true, origin:'http://localhost:3000'}));
 app.use(express.json());
 
 mongoose.connect('mongodb+srv://toastietoes:90lYNFRO9J9IEZdA@cluster0.qc6lhkm.mongodb.net/?retryWrites=true&w=majority')
@@ -30,7 +32,14 @@ app.post('/login', async (req,res) => {
     const {username, password} = req.body;
     const userDoc = await User.findOne({username});
     const passOk = bcrypt.compareSync(password, userDoc.password);
-    res.json(passOk)
+    if (passOk) {
+        jwt.sign({username, id:userDoc._id}, secret, {}, (err, token) => {
+            if (err) throw err;
+            res.cookie('token', token).json('ok');
+        });
+    } else {
+        res.status(400).json('wrong credentials');
+    }
 })
 app.listen(4000);
 
